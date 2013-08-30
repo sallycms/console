48ce0f304bc9279c6b02c457758042ad3ba7c755
Christoph <github@xrstf.de>
2013-08-30 03:31:26 +0200

    this is 0.10 from now on

---
+++ lib/sly/Console/Command/ClearCache.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright (c) 2013, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+namespace sly\Console\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClearCache extends Base {
+	protected function configure() {
+		$this
+			->setName('sly:cache:clear')
+			->setDescription('Clears the system cache.')
+			->setDefinition(array(
+				new InputOption('all', null, InputOption::VALUE_NONE, 'To perform all optional tasks as well.'),
+				new InputOption('sync-develop', 'd', InputOption::VALUE_NONE, 'To re-sync templates and modules.')
+			));
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$container = $this->getContainer();
+		$all       = $input->getOption('all');
+
+		// clear our own data caches
+		$output->write('Flushing data cache...');
+		$container['sly-cache']->clear('sly', true);
+		$output->writeln(' <info>done</info>.');
+
+		// sync develop files
+		if ($all || $input->getOption('sync-develop')) {
+			$output->write('Refreshing development contents...');
+			$container['sly-service-template']->refresh();
+			$container['sly-service-module']->refresh();
+			$output->writeln(' <info>done</info>.');
+		}
+
+		$container['sly-dispatcher']->notify('SLY_CACHE_CLEARED', null, array(
+			'backend'   => false,
+			'container' => $container
+		));
+	}
+}
---
+++ lib/sly/Console/NullSession.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright (c) 2013, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+namespace sly\Console;
+
+use sly_Session;
+
+class NullSession extends sly_Session {
+	public function __construct() {
+		parent::__construct('');
+	}
+
+	public function getID() {
+		return false;
+	}
+
+	public function getInstallID() {
+		return '';
+	}
+
+	public function get($key, $type, $default = null) {
+		return $default;
+	}
+
+	public function set($key, $value) {
+		// do nothing
+	}
+
+	public function delete($key) {
+		// do nothing
+	}
+
+	public function flush() {
+		// do nothing
+	}
+
+	public function destroy() {
+		// do nothing
+	}
+
+	public function regenerateID() {
+		// do nothing
+	}
+}
